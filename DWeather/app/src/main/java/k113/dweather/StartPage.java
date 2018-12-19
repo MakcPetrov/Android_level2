f9db7e9a0c4cfed451951438ba7d73acc0e30a7f
MakcPetrov <k-113@yandex.ru>
2018-12-19 22:29:11 +0300

    13

--- DWeather/app/src/main/java/k113/dweather/StartPage.java
+++ DWeather/app/src/main/java/k113/dweather/StartPage.java
@@ -3,7 +3,11 @@ package k113.dweather;
 import android.app.Activity;
 import android.app.Application;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.NavigationView;
 import android.support.design.widget.Snackbar;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
@@ -15,9 +19,10 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 
-public class StartPage extends AppCompatActivity {
+public class StartPage extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener{
     Toolbar toolbar;
     String [] cityes = {"Москва", "Питер", "Новосибирск", "Екатеринбург", "Урюпинск", "Гадюкино"};//города TODO: обмен с BackEnd
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
 
@@ -43,7 +48,18 @@ public class StartPage extends AppCompatActivity {
 //                    Snackbar.LENGTH_LONG).show();
 //            }
 //        });//SetOnItemClickListener
-    }
+
+        DrawerLayout drawer = findViewById(R.id.drawer_layout);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawer.addDrawerListener(toggle);
+        toggle.syncState();
+
+        NavigationView navigationView = findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
+
+
+    }//onCreate
 
 // скрипач не нужен
 //        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
@@ -102,5 +118,15 @@ public class StartPage extends AppCompatActivity {
                 return true;
         }
         return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        return false;
+    }
+
+    @Override
+    public void onPointerCaptureChanged(boolean hasCapture) {
+
     }
 }
